e60a76e0f4fe1ed3e43d002c3ea62ab9e09a3b2e
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-20 15:34:18 +0900

    feat: codeup 1014

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1014.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char val1 = sc.next().charAt(0);
+        char val2 = sc.next().charAt(0);
+        System.out.printf("%c %c",val2, val1);
+    }
+}
