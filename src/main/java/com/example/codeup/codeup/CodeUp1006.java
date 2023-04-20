335354656c036e47e452c828edf654231531a7df
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-20 10:17:42 +0900

    codeup 1006-1008

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1006.java
@@ -0,0 +1,9 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1006 {
+    public static void main(String[] args) {
+        System.out.println("\"!@#$%%^&*()\"");
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/Codeup100.java
@@ -0,0 +1,7 @@
+package com.example.codeup.codeup;
+
+public class Codeup100 {
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }
+}
