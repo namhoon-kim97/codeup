e350f61100af672acd34b3b781b05ec7f340d764
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-21 11:03:16 +0900

    feat: codeup 1018

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1018.java
@@ -0,0 +1,10 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%s", sc.nextLine());
+    }
+}
