e0430f743c44ea20cb58b8257d39fcb49d46ae65
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-24 15:09:21 +0900

    feat: codeup 1019 - 1023

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1020.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nums = sc.nextLine();
+        String[] numsSplit = nums.split("-");
+        System.out.printf("%s%s", numsSplit[0], numsSplit[1]);
+    }
+}
