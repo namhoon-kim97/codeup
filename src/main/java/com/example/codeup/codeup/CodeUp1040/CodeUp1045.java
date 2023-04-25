2190b7fb524cbea2177302798cb75c48fbc64a56
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-25 15:34:35 +0900

    feat: rename Codeup1045 -> CodeUp1045

--- src/main/java/com/example/codeup/codeup/CodeUp1040/CodeUp1045.java
+++ src/main/java/com/example/codeup/codeup/CodeUp1040/CodeUp1045.java
@@ -1,7 +1,7 @@
 package com.example.codeup.codeup.CodeUp1040;
 
 import java.util.Scanner;
-public class Codeup1045 {
+public class CodeUp1045 {
     public static void main(String[] args) {
         Scanner sc  =new Scanner(System.in);
         long val1 = sc.nextLong();
