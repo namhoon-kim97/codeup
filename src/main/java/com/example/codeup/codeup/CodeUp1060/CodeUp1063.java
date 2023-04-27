0433724de1efd79ce9279cba902951c942e189a2
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-27 16:08:15 +0900

    feat: CodeUp 1063-1064

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1060/CodeUp1063.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup.CodeUp1060;
+
+import java.util.Scanner;
+
+public class CodeUp1063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(val1 > val2 ? val1 : val2);
+    }
+}
