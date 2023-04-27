b9bdb770f8aa6d235632a9de27ed7fe0de9f8d79
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-27 15:01:02 +0900

    feat: Codeup 1059 - 1062

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1050/CodeUp1059.java
@@ -0,0 +1,10 @@
+package com.example.codeup.codeup.CodeUp1050;
+import java.util.Scanner;
+
+public class CodeUp1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        System.out.printf("%d\n", ~val1);
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/CodeUp1060/CodeUp1062.java
@@ -0,0 +1,12 @@
+package com.example.codeup.codeup.CodeUp1060;
+
+import java.util.Scanner;
+
+public class CodeUp1062 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.printf("%d\n", val1 ^ val2);
+    }
+}
