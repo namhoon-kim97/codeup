7e239dbb22b050816f47b5f8f6b2ee095caaf3dc
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-27 13:29:56 +0900

    feat: Codeup 1053 - 1058

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1050/CodeUp1053.java
@@ -0,0 +1,13 @@
+package com.example.codeup.codeup.CodeUp1050;
+import java.util.Scanner;
+
+public class CodeUp1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num == 1)
+            System.out.println(0);
+        else
+            System.out.println(1);
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/CodeUp1050/CodeUp1057.java
@@ -0,0 +1,14 @@
+package com.example.codeup.codeup.CodeUp1050;
+import java.util.Scanner;
+
+public class CodeUp1057 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        if(val1 == val2)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
