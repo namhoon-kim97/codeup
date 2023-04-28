0b1ab51a083628174af9fb8b617ab2e551306b98
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-28 13:04:09 +0900

    feat: CodeUp 1065-1070

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1060/CodeUp1065.java
@@ -0,0 +1,18 @@
+package com.example.codeup.codeup.CodeUp1060;
+
+import java.util.Scanner;
+
+public class CodeUp1065 {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int val1 = sc.nextInt();
+       int val2 = sc.nextInt();
+       int val3 = sc.nextInt();
+       if(val1 % 2 == 0)
+           System.out.println(val1);
+       if(val2 % 2 == 0)
+           System.out.println(val2);
+       if(val3 % 2 == 0)
+           System.out.println(val3);
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/CodeUp1060/CodeUp1066.java
@@ -0,0 +1,24 @@
+package com.example.codeup.codeup.CodeUp1060;
+
+import java.util.Scanner;
+
+public class CodeUp1066 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        int val3 = sc.nextInt();
+        if(val1 % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+        if(val2 % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+        if(val3 % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+}
