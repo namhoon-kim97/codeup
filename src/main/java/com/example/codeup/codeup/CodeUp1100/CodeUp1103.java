70f295504be3c67f7bfd300e9086229c5e54ade3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-01 09:16:41 +0900

    feat: CodeUp 1103, 1106

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1100/CodeUp1103.java
@@ -0,0 +1,7 @@
+package com.example.codeup.codeup.CodeUp1100;
+
+public class CodeUp1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/com/example/codeup/codeup/CodeUp1100/CodeUp1106.java
@@ -0,0 +1,7 @@
+package com.example.codeup.codeup.CodeUp1100;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        System.out.printf("%d %d", Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
