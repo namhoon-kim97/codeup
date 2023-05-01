9cabe612d0af489e076c62c2e0e526513b54751f
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-01 10:30:14 +0900

    feat: CodeUp 1071

---
+++ src/main/java/com/example/codeup/codeup/CodeUp1100/CodeUp1071.java
@@ -0,0 +1,14 @@
+package com.example.codeup.codeup.CodeUp1100;
+
+import java.util.Scanner;
+
+public class CodeUp1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        while(val != 0){
+            System.out.println(val);
+            val = sc.nextInt();
+        }
+    }
+}
